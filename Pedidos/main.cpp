71317d8e8475d48e47360a40c95c5d79e1b28a3f
cdevelopment <cdevelopment@NotebookLuis>
2019-11-06 18:13:24 -0300

    Primera modificación

---
+++ Pedidos/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: cdevelopment
+ *
+ * Created on 6 de noviembre de 2019, 18:10
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hola Grupo de Desertores de la Eleccion";
+    return 0;
+}
+
